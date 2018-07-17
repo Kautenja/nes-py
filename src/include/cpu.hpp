d2623f9c5c56c6ca13c797523d0df92e1a5d94f2
Kautenja <ckfour@icloud.com>
2018-07-17 01:40:28 -0500

    cleanup

--- src/include/cpu.hpp
+++ src/include/cpu.hpp
@@ -3,26 +3,47 @@
 
 namespace CPU {
 
-enum IntType { NMI, RESET, IRQ, BRK };  // Interrupt type.
-typedef u16 (*Mode)(void);              // Addressing mode.
+    // Interrupt type
+    enum IntType { NMI, RESET, IRQ, BRK };
+    // Addressing mode
+    typedef u16 (*Mode)(void);
 
-/* Processor flags */
-enum Flag {C, Z, I, D, V, N};
-class Flags
-{
-    bool f[6];
+    /* Processor flags */
+    enum Flag {C, Z, I, D, V, N};
+    class Flags {
+        // a private data structure to hold the flags
+        bool f[6];
 
-  public:
-    bool& operator[] (const int i) { return f[i]; }
+    public:
 
-    u8 get() { return f[C] | f[Z] << 1 | f[I] << 2 | f[D] << 3 | 1 << 5 | f[V] << 6 | f[N] << 7; }
-    void set(u8 p) { f[C] = NTH_BIT(p, 0); f[Z] = NTH_BIT(p, 1); f[I] = NTH_BIT(p, 2);
-                     f[D] = NTH_BIT(p, 3); f[V] = NTH_BIT(p, 6); f[N] = NTH_BIT(p, 7); }
-};
+        bool& operator[] (const int i) { return f[i]; }
 
-void set_nmi(bool v = true);
-void set_irq(bool v = true);
-void power();
-void run_frame();
+        /** Return the flags as a byte */
+        u8 get() {
+            return f[C] |
+                f[Z] << 1 |
+                f[I] << 2 |
+                f[D] << 3 |
+                1 << 5 |
+                f[V] << 6 |
+                f[N] << 7;
+        }
+
+        /** Set the flags from a full byte */
+        void set(u8 p) {
+            f[C] = NTH_BIT(p, 0);
+            f[Z] = NTH_BIT(p, 1);
+            f[I] = NTH_BIT(p, 2);
+            f[D] = NTH_BIT(p, 3);
+            f[V] = NTH_BIT(p, 6);
+            f[N] = NTH_BIT(p, 7);
+        }
+
+    };
+
+    void set_nmi(bool v = true);
+    void set_irq(bool v = true);
+    void power();
+    void run_frame();
 
 }

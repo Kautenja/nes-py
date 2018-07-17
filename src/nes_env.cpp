c90b57b83b8369e2628e48705a6c1940c1230b79
Kautenja <kautencreations@gmail.com>
2018-07-17 17:20:05 -0500

    update with init method

--- src/nes_env.cpp
+++ src/nes_env.cpp
@@ -3,22 +3,32 @@
 #include "gui.hpp"
 
 
-class Foo{
-    public:
-        void bar(wchar_t* path){
-            std::wcout << path << std::endl;
-            // convert the wchar_t type to a string
-            std::wstring ws(path);
-            std::string str(ws.begin(), ws.end());
-            // initialize the GUI with the given ROM path
-            GUI::init(str);
-            // run the GUI
-            GUI::run();
-        }
+class NESEnv{
+
+private:
+
+public:
+    NESEnv(wchar_t* path) {
+        // convert the wchar_t type to a string
+        std::wstring ws(path);
+        std::string str(ws.begin(), ws.end());
+    }
+
+    // void bar(wchar_t* path){
+    //     std::wcout << path << std::endl;
+    //     // convert the wchar_t type to a string
+    //     std::wstring ws(path);
+    //     std::string str(ws.begin(), ws.end());
+    //     // initialize the GUI with the given ROM path
+    //     GUI::init(str);
+    //     // run the GUI
+    //     GUI::run();
+    // }
 };
 
 
 extern "C" {
-    Foo* Foo_new(){ return new Foo(); }
-    void Foo_bar(Foo* foo, wchar_t* path){ foo->bar(path); }
+    NESEnv* NESEnv_init(wchar_t* path){ return new NESEnv(path); }
+
+    // void Foo_bar(Foo* foo, wchar_t* path){ foo->bar(path); }
 }

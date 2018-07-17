c81e0bdfa15779b3cc2f5cd0c64384a3ff65aa9f
Kautenja <ckfour@icloud.com>
2018-07-17 02:59:09 -0500

    hello, world for wrapper

--- src/lainesmodule.cpp
+++ src/lainesmodule.cpp
@@ -1,20 +1,14 @@
-// #include <Python.h>
+#include <iostream>
+
+class Foo{
+    public:
+        void bar(){
+            std::cout << "Hello" << std::endl;
+        }
+};
 
 
-// static PyObject * laines_init(PyObject *self, PyObject *args) {
-//     const char *command;
-//     int sts;
-
-//     if (!PyArg_ParseTuple(args, "s", &command))
-//         return NULL;
-//     sts = system(command);
-//     return Py_BuildValue("i", sts);
-// }
-
-// static PyObject * laines_reset(PyObject *self) {
-
-// }
-
-// static PyObject * laines_step(PyObject *self, PyObject *args) {
-
-// }
+extern "C" {
+    Foo* Foo_new(){ return new Foo(); }
+    void Foo_bar(Foo* foo){ foo->bar(); }
+}

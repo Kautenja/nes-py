652d7f8199043e5593e151dbda0d11f32c25d3f2
Kautenja <ckfour@icloud.com>
2018-07-18 17:38:46 -0500

    create application

---
+++ nes_py/_app/cli.py
@@ -0,0 +1,41 @@
+"""NES emulator for OpenAI Gym."""
+import argparse
+from .play import play_human, play_random
+from ..nes_env import NesENV
+
+
+def create_argparser() -> argparse.ArgumentParser:
+    """Create and return an argument parser for this command line interface."""
+    parser = argparse.ArgumentParser(description=__doc__)
+    # add the argument for the Super Mario Bros environment to run
+    parser.add_argument('--rom', '-r',
+        type=str,
+        help='The path to the ROM to play.'
+    )
+    # add the argument for the mode of execution as either human or random
+    parser.add_argument('--mode', '-m',
+        type=str,
+        default='human',
+        choices=['human', 'random'],
+        help='The execution mode for the emulation.'
+    )
+
+    return parser
+
+
+def main() -> None:
+    """The main entry point for the command line interface."""
+    # parse arguments from the command line (argparse validates arguments)
+    args = create_argparser().parse_args()
+    # select the method for playing the game
+    if args.mode == 'human':
+        play = play_human
+    elif args.mode == 'random':
+        play = play_random
+    # play the game
+    env = NesENV(args.rom)
+    play(env)
+
+
+# explicitly define the outward facing API of this module
+__all__ = [main.__name__]

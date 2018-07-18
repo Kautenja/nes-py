b6d8530126677722ea75a52604e0e9d053226010
Kautenja <ckfour@icloud.com>
2018-07-18 17:43:14 -0500

    update main to use the CLI

--- __main__.py
+++ __main__.py
@@ -1,29 +1,6 @@
 """The main script for development testing."""
-import sys
-from tqdm import tqdm
-from nes_py.nes_env import NesENV
-import numpy as np
-
-# get the path for the ROM from the command line
-path = sys.argv[1]
+from nes_py._app.cli import main
 
 
-# create the environment
-env = NesENV(path)
-
-
-# run through some random steps in the environment
-try:
-    done = True
-    for _ in tqdm(range(500)):
-        if done:
-            _ = env.reset()
-        action = env.action_space.sample()
-        _, reward, done, _ = env.step(action)
-        env.render()
-except KeyboardInterrupt:
-    pass
-
-
-# close the environment
-env.close()
+# execute the main entry point of the CLI
+main()

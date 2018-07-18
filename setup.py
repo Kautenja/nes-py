c94d67acb8067399c23468c6fd99afe2ab3b9342
Kautenja <ckfour@icloud.com>
2018-07-18 16:57:54 -0500

    update setup script

--- setup.py
+++ setup.py
@@ -8,49 +8,44 @@ def README():
         return README_file.read()
 
 
-# setup(
-#     name='nes_py',
-#     setup_requires=[
-#         'very-good-setuptools-git-version'
-#     ],
-#     version_format='{tag}',
-#     description='An NES Emulator for Python3 clients',
-#     long_description=README(),
-#     long_description_content_type='text/markdown',
-#     keywords='NES Emulator',
-#     classifiers=[
-#         'Development Status :: 1 - Planning',
-#         'Intended Audience :: Developers',
-#         'Intended Audience :: Science/Research',
-#         'License :: OSI Approved :: MIT License',
-#         'Operating System :: MacOS :: MacOS X',
-#         'Operating System :: POSIX :: Linux',
-#         'Programming Language :: Python :: 3.5',
-#         'Programming Language :: Python :: 3.6',
-#         'Programming Language :: Python :: 3.7',
-#         'Topic :: Games/Entertainment',
-#         'Topic :: Software Development :: Libraries :: Python Modules',
-#         'Topic :: System :: Emulators',
-#     ],
-#     url='https://github.com/Kautenja/nes-py',
-#     author='Christian Kauten',
-#     author_email='kautencreations@gmail.com',
-#     license='MIT',
-#     packages=find_packages(),
-#     zip_safe=False,
-#     extras_require={
-#         'development': [
-#             'numpy>=1.14.2',
-#             'Pillow==5.0.0',
-#         ]
-#     },
-#     # install_requires=[
-#     #     'gym>=0.10.5',
-#     #     'numpy>=1.14.2',
-#     # ],
-#     entry_points={
-#         'console_scripts': [
-#             'nes_py = nes_py._app.cli:main',
-#         ],
-#     },
-# )
+setup(
+    name='nes_py',
+    setup_requires=[
+        'very-good-setuptools-git-version'
+    ],
+    version_format='{tag}',
+    description='An NES Emulator and OpenAI Gym interface',
+    long_description=README(),
+    long_description_content_type='text/markdown',
+    keywords='NES Emulator OpenAI-Gym',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Games/Entertainment',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Emulators',
+    ],
+    url='https://github.com/Kautenja/nes-py',
+    author='Christian Kauten',
+    author_email='kautencreations@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+    # install_requires=[
+    #     'gym>=0.10.5',
+    #     'numpy>=1.14.2',
+    #     'pyglet>=1.3.2',
+    # ],
+    entry_points={
+        'console_scripts': [
+            'nes_py = nes_py._app.cli:main',
+        ],
+    },
+)

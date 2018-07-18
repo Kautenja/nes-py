d89c1c7ca8d73095f88453d5bf870fa4ad3290e9
Kautenja <kautencreations@gmail.com>
2018-07-17 20:27:08 -0500

    update Joypad to use virtual controller

--- src/include/joypad.hpp
+++ src/include/joypad.hpp
@@ -3,6 +3,7 @@
 
 namespace Joypad {
 
+    void write_buttons(int n, u8 buttons);
     u8 read_state(int n);
     void write_strobe(bool v);
 
--- src/joypad.cpp
+++ src/joypad.cpp
@@ -1,18 +1,37 @@
 #include "gui.hpp"
 
+/// Control for the emulator using virtual controllers
 namespace Joypad {
 
+    /// Virtual joypad with values from Python API
+    u8 joypad_buttons[2];
     /// Joypad shift registers.
     u8 joypad_bits[2];
     /// Joypad strobe latch.
     bool strobe;
 
-    /// Read joypad state (NES register format)
-    u8 read_state(int n)
-    {
+    /**
+        Write a button state to the given joypad.
+
+        @param n the joypad to address. 0 for player 1, 1 for player 2
+        @param buttons the bitmap of pressed buttons on the controller
+
+    */
+    void write_buttons(int n, u8 buttons) {
+        joypad_buttons[n] = buttons;
+    }
+
+    /**
+        Read joypad state (NES register format).
+
+        @param n the joypad to address. 0 for player 1, 1 for player 2
+        @returns the byte representing the controller state
+
+    */
+    u8 read_state(int n) {
         // When strobe is high, it keeps reading A:
         if (strobe)
-            return 0x40 | (GUI::get_joypad_state(n) & 1);
+            return 0x40 | (joypad_buttons[n] & 1);
 
         // Get the status of a button and shift the register:
         u8 j = 0x40 | (joypad_bits[n] & 1);
@@ -20,13 +39,18 @@ namespace Joypad {
         return j;
     }
 
-    /// Write joypad strobe
-    void write_strobe(bool v)
-    {
+    /**
+        Write joypad strobe flag.
+
+        @param v whether strobe is enabled (true) or disabled (false)
+
+    */
+    void write_strobe(bool v) {
         // Read the joypad data on strobe's transition 1 -> 0.
         if (strobe and !v)
-            for (int i = 0; i < 2; i++)
-                joypad_bits[i] = GUI::get_joypad_state(i);
+            for (int i = 0; i < 2; i++) {
+                joypad_bits[i] = joypad_buttons[i];
+            }
 
         strobe = v;
     }

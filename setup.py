7c13b0eea2f423897d4bca268aac10fd3fa2016c
Kautenja <ckfour@icloud.com>
2018-07-21 01:28:14 -0500

    classifiers

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: POSIX :: Linux',
+        'Programming Language :: C++',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',

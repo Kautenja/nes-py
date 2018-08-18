6a45f95669d5cb874fb454b6cf7b166f4acc2626
Kautenja <ckfour@icloud.com>
2018-08-17 22:54:10 -0500

    experimental setup

--- nes_py/laines/nes_env.cpp
+++ nes_py/laines/nes_env.cpp
@@ -41,65 +41,6 @@ void NESEnv::restore() {
     current_state->load();
 }
 
-// definitions of functions for the Python interface to access
-extern "C" {
-    /// The initializer to return a new NESEnv with a given path.
-    NESEnv* NESEnv_init(wchar_t* path){
-        return new NESEnv(path);
-    }
-
-    /// The width of the NES screen.
-    unsigned NESEnv_width() {
-        return GUI::get_width();
-    }
-
-    /// The height of the NES screen.
-    unsigned NESEnv_height() {
-        return GUI::get_height();
-    }
-
-    /// The getter for RAM access
-    u8 NESEnv_read_mem(NESEnv* env, u16 address) {
-        return CPU::read_mem(address);
-    }
-
-    /// The setter for RAM access
-    void NESEnv_write_mem(NESEnv* env, u16 address, u8 value) {
-        CPU::write_mem(address, value);
-    }
-
-    /// Copy the screen of the emulator to an output buffer (NumPy array)
-    void NESEnv_screen(NESEnv* env, unsigned char *output_buffer) {
-        PPU::get_gui()->copy_screen(output_buffer);
-    }
-
-    /// The function to reset the environment.
-    void NESEnv_reset(NESEnv* env) {
-        env->reset();
-    }
-
-    /// The function to perform a step on the emulator.
-    void NESEnv_step(NESEnv* env, unsigned char action) {
-        env->step(action);
-    }
-
-    /// The function to destroy an NESEnv and clear it from memory.
-    void NESEnv_close(NESEnv* env) {
-        delete env;
-    }
-
-    /// The function to backup the game-state
-    void NESEnv_backup(NESEnv* env) {
-        env->backup();
-    }
-
-    /// The function to restore the game-state
-    void NESEnv_restore(NESEnv* env) {
-        env->restore();
-    }
-
-}
-
 // #if defined(_WIN32) || defined(WIN32) || defined(__CYGWIN__) || defined(__MINGW32__) || defined(__BORLANDC__)
 // #include <Python.h>
 
@@ -165,4 +106,63 @@ void PyInit_lib_nes_env() { }
     void NESEnv_restore(NESEnv* env) {
         env->restore();
     }
+#else
+// definitions of functions for the Python interface to access
+extern "C" {
+    /// The initializer to return a new NESEnv with a given path.
+    NESEnv* NESEnv_init(wchar_t* path){
+        return new NESEnv(path);
+    }
+
+    /// The width of the NES screen.
+    unsigned NESEnv_width() {
+        return GUI::get_width();
+    }
+
+    /// The height of the NES screen.
+    unsigned NESEnv_height() {
+        return GUI::get_height();
+    }
+
+    /// The getter for RAM access
+    u8 NESEnv_read_mem(NESEnv* env, u16 address) {
+        return CPU::read_mem(address);
+    }
+
+    /// The setter for RAM access
+    void NESEnv_write_mem(NESEnv* env, u16 address, u8 value) {
+        CPU::write_mem(address, value);
+    }
+
+    /// Copy the screen of the emulator to an output buffer (NumPy array)
+    void NESEnv_screen(NESEnv* env, unsigned char *output_buffer) {
+        PPU::get_gui()->copy_screen(output_buffer);
+    }
+
+    /// The function to reset the environment.
+    void NESEnv_reset(NESEnv* env) {
+        env->reset();
+    }
+
+    /// The function to perform a step on the emulator.
+    void NESEnv_step(NESEnv* env, unsigned char action) {
+        env->step(action);
+    }
+
+    /// The function to destroy an NESEnv and clear it from memory.
+    void NESEnv_close(NESEnv* env) {
+        delete env;
+    }
+
+    /// The function to backup the game-state
+    void NESEnv_backup(NESEnv* env) {
+        env->backup();
+    }
+
+    /// The function to restore the game-state
+    void NESEnv_restore(NESEnv* env) {
+        env->restore();
+    }
+
+}
 #endif

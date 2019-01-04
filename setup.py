20a5df9bc451c6039d7d6aa4d0bd2c267312e43b
Kautenja <kautencreations@gmail.com>
2019-01-04 16:13:22 -0600

    dev status stable

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     long_description_content_type='text/markdown',
     keywords='NES Emulator OpenAI-Gym',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',

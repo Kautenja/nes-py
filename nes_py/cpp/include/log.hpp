a003f1d00e9cfa7f6ce40cd5a0d0ccba4db6f740
Kautenja <kautencreations@gmail.com>
2019-01-04 15:17:49 -0600

    fix logging

--- nes_py/cpp/include/log.hpp
+++ nes_py/cpp/include/log.hpp
@@ -6,17 +6,11 @@
 #include <memory>
 #include <cstring>
 
-#ifndef __FILENAME__
-#define __FILENAME__ __FILE__
-#endif
+#define debug_disabled true
 
 #define LOG(level) \
-if (level > Log::get().getLevel()) ; \
-else Log::get().getStream() << '[' << __FILENAME__ << ":" << std::dec << __LINE__ << "] "
-
-#define LOG_CPU \
-if (CpuTrace != Log::get().getLevel()) ; \
-else Log::get().getCpuTraceStream()
+    if (debug_disabled) {} \
+    else std::cerr
 
 enum Level
 {
@@ -25,54 +19,6 @@ enum Level
     Info,
     InfoVerbose,
     CpuTrace
-};
-
-class Log
-{
-public:
-    ~Log();
-    void setLogStream(std::ostream& stream);
-    void setCpuTraceStream(std::ostream& stream);
-    Log& setLevel(Level level);
-    Level getLevel();
-
-    std::ostream& getStream();
-    std::ostream& getCpuTraceStream();
-
-    static Log& get();
-private:
-    Level m_logLevel;
-    std::ostream* m_logStream;
-    std::ostream* m_cpuTrace;
-    static std::unique_ptr<Log> m_instance;
-};
-
-//Courtesy of http://wordaligned.org/articles/cpp-streambufs#toctee-streams
-class TeeBuf : public std::streambuf
-{
-    public:
-        // Construct a streambuf which tees output to both input
-        // streambufs.
-        TeeBuf(std::streambuf* sb1, std::streambuf* sb2);
-    private:
-        // This tee buffer has no buffer. So every character "overflows"
-        // and can be put directly into the teed buffers.
-        virtual int overflow(int c);
-        // Sync both teed buffers.
-        virtual int sync();
-    private:
-        std::streambuf* m_sb1;
-        std::streambuf* m_sb2;
-};
-
-class TeeStream : public std::ostream
-{
-    public:
-        // Construct an ostream which tees output to the supplied
-        // ostreams.
-        TeeStream(std::ostream& o1, std::ostream& o2);
-    private:
-        TeeBuf m_tbuf;
 };
 
 #endif // LOG_H

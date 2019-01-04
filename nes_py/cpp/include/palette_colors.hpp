ae743f5a8b5bfce2b15daa6e1c19ff79f85e06d6
Kautenja <kautencreations@gmail.com>
2019-01-04 17:03:50 -0600

    add source info for color palette

--- nes_py/cpp/include/palette_colors.hpp
+++ nes_py/cpp/include/palette_colors.hpp
@@ -1,6 +1,7 @@
 #include <cstdint>
 
 // Colors in xRGB
+// source: http://www.thealmightyguru.com/Games/Hacking/Wiki/index.php/NES_Palette
 const std::uint32_t colors[] = {
     0x7C7C7C, 0x0000FC, 0x0000BC, 0x4428BC, 0x940084, 0xA80020, 0xA81000, 0x881400,
     0x503000, 0x007800, 0x006800, 0x005800, 0x004058, 0x000000, 0x000000, 0x000000,

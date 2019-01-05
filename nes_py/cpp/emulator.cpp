61ea7ed49d61c6413aa8c5244019b0bd209a2e62
Kautenja <kautencreations@gmail.com>
2019-01-04 23:07:17 -0600

    cleanup

--- nes_py/cpp/emulator.cpp
+++ nes_py/cpp/emulator.cpp
@@ -1,7 +1,5 @@
 #include "emulator.hpp"
 #include "log.hpp"
-#include <thread>
-#include <chrono>
 
 Emulator::Emulator(std::string rom_path) : cpu(), ppu(), rom_path(rom_path) {
     // raise an error if IO callback setup fails
@@ -34,14 +32,42 @@ Emulator::Emulator(std::string rom_path) : cpu(), ppu(), rom_path(rom_path) {
 
 Emulator::Emulator(Emulator* emulator) {
     rom_path = emulator->rom_path;
-    bus = emulator->bus;
-    picture_bus = emulator->picture_bus;
-    cartridge = emulator->cartridge;
-    // mapper = emulator->mapper;
     controller1 = emulator->controller1;
     controller2 = emulator->controller2;
     cpu = emulator->cpu;
     ppu = emulator->ppu;
+    bus = emulator->bus;
+    picture_bus = emulator->picture_bus;
+    cartridge = emulator->cartridge;
+    // mapper = emulator->mapper;
+    // bus.setMapper(mapper);
+    // picture_bus.setMapper(mapper);
+    // raise an error if IO callback setup fails
+    if(
+        !bus.setReadCallback(PPUSTATUS, [&](void) {return ppu.getStatus();}) ||
+        !bus.setReadCallback(PPUDATA, [&](void) {return ppu.getData(picture_bus);}) ||
+        !bus.setReadCallback(JOY1, [&](void) {return controller1.read();}) ||
+        !bus.setReadCallback(JOY2, [&](void) {return controller2.read();}) ||
+        !bus.setReadCallback(OAMDATA, [&](void) {return ppu.getOAMData();})
+    ) {
+        LOG(Error) << "Critical error: Failed to set I/O callbacks" << std::endl;
+    }
+    // raise an error if IO callback setup fails
+    if(
+        !bus.setWriteCallback(PPUCTRL, [&](Byte b) {ppu.control(b);}) ||
+        !bus.setWriteCallback(PPUMASK, [&](Byte b) {ppu.setMask(b);}) ||
+        !bus.setWriteCallback(OAMADDR, [&](Byte b) {ppu.setOAMAddress(b);}) ||
+        !bus.setWriteCallback(PPUADDR, [&](Byte b) {ppu.setDataAddress(b);}) ||
+        !bus.setWriteCallback(PPUSCROL, [&](Byte b) {ppu.setScroll(b);}) ||
+        !bus.setWriteCallback(PPUDATA, [&](Byte b) {ppu.setData(picture_bus, b);}) ||
+        !bus.setWriteCallback(OAMDMA, [&](Byte b) {DMA(b);}) ||
+        !bus.setWriteCallback(JOY1, [&](Byte b) {controller1.strobe(b); controller2.strobe(b);}) ||
+        !bus.setWriteCallback(OAMDATA, [&](Byte b) {ppu.setOAMData(b);})
+    ) {
+        LOG(Error) << "Critical error: Failed to set I/O callbacks" << std::endl;
+    }
+    // set the interrupt callback for the PPU
+    ppu.setInterruptCallback([&](){ cpu.interrupt(bus, CPU::NMI); });
 }
 
 void Emulator::loadRom() {
@@ -84,11 +110,7 @@ void Emulator::step(unsigned char action) {
     controller1.write_buttons(action);
     // approximate a frame
     for (int i = 0; i < 29781; i++) {
-        // PPU steps 3 times per clock cycle
-        ppu.step(picture_bus);
-        ppu.step(picture_bus);
-        ppu.step(picture_bus);
-        // CPU steps once per clock cycle (obviously)
+        ppu.cpu_step(picture_bus);
         cpu.step(bus);
     }
 }

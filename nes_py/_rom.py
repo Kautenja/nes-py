383ae3e3a86d30312843826a0bea32711c372dcf
Kautenja <kautencreations@gmail.com>
2019-01-05 22:47:51 -0600

    remove type hints

--- nes_py/_rom.py
+++ nes_py/_rom.py
@@ -13,12 +13,12 @@ class ROM(object):
     # It spells "NES<END>"
     _MAGIC = bytes([0x4E, 0x45, 0x53, 0x1A])
 
-    def __init__(self, rom_path: str) -> None:
+    def __init__(self, rom_path) -> None:
         """
         Initialize a new ROM.
 
         Args:
-            rom_path: the path to the ROM file
+            rom_path (str): the path to the ROM file
 
         Returns:
             None
@@ -45,37 +45,37 @@ class ROM(object):
     #
 
     @property
-    def header(self) -> bytes:
-        """Return the header of the ROM file."""
+    def header(self):
+        """Return the header of the ROM file as bytes."""
         return self.raw_data[:16]
 
     @property
-    def _magic(self) -> bytes:
+    def _magic(self):
         """Return the magic bytes in the first 4 bytes."""
         return self.header[:4]
 
     @property
-    def prg_rom_size(self) -> int:
+    def prg_rom_size(self):
         """Return the size of the PRG ROM in KB."""
         return 16 * self.header[4]
 
     @property
-    def chr_rom_size(self) -> int:
+    def chr_rom_size(self):
         """Return the size of the CHR ROM in KB."""
         return 8 * self.header[5]
 
     @property
-    def flags_6(self) -> str:
+    def flags_6(self):
         """Return the flags at the 6th byte of the header."""
         return '{:08b}'.format(self.header[6])
 
     @property
-    def flags_7(self) -> str:
+    def flags_7(self):
         """Return the flags at the 7th byte of the header."""
         return '{:08b}'.format(self.header[7])
 
     @property
-    def prg_ram_size(self) -> int:
+    def prg_ram_size(self):
         """Return the size of the PRG RAM in KB."""
         size = self.header[8]
         # size becomes 8 when it's zero for compatibility
@@ -85,12 +85,12 @@ class ROM(object):
         return 8 * size
 
     @property
-    def flags_9(self) -> str:
+    def flags_9(self):
         """Return the flags at the 9th byte of the header."""
         return '{:08b}'.format(self.header[9])
 
     @property
-    def flags_10(self) -> str:
+    def flags_10(self):
         """
         Return the flags at the 10th byte of the header.
 
@@ -102,7 +102,7 @@ class ROM(object):
         return '{:08b}'.format(self.header[10])
 
     @property
-    def _zero_fill(self) -> int:
+    def _zero_fill(self):
         """Return the zero fill bytes at the end of the header."""
         return int.from_bytes(self.header[11:], byteorder='big')
 
@@ -111,33 +111,33 @@ class ROM(object):
     #
 
     @property
-    def mapper(self) -> int:
+    def mapper(self):
         """Return the mapper number this ROM uses."""
         # the high nibble is in flags 7, the low nibble is in flags 6
         return int(self.flags_7[:4] + self.flags_6[:4], 2)
 
     @property
-    def is_ignore_mirroring(self) -> bool:
+    def is_ignore_mirroring(self):
         """Return a boolean determining if the ROM ignores mirroring."""
         return bool(int(self.flags_6[4]))
 
     @property
-    def has_trainer(self) -> bool:
+    def has_trainer(self):
         """Return a boolean determining if the ROM has a trainer block."""
         return bool(int(self.flags_6[5]))
 
     @property
-    def has_battery_backed_ram(self) -> bool:
+    def has_battery_backed_ram(self):
         """Return a boolean determining if the ROM has a battery-backed RAM."""
         return bool(int(self.flags_6[6]))
 
     @property
-    def is_vertical_mirroring(self) -> bool:
+    def is_vertical_mirroring(self):
         """Return the mirroring mode this ROM uses."""
         return bool(int(self.flags_6[7]))
 
     @property
-    def has_play_choice_10(self) -> bool:
+    def has_play_choice_10(self):
         """
         Return whether this cartridge uses PlayChoice-10.
 
@@ -149,7 +149,7 @@ class ROM(object):
         return bool(int(self.flags_7[6]))
 
     @property
-    def has_vs_unisystem(self) -> bool:
+    def has_vs_unisystem(self):
         """
         Return whether this cartridge has VS Uni-system.
 
@@ -161,7 +161,7 @@ class ROM(object):
         return bool(int(self.flags_7[7]))
 
     @property
-    def is_pal(self) -> bool:
+    def is_pal(self):
         """Return the TV system this ROM supports."""
         return bool(int(self.flags_9[7]))
 
@@ -170,12 +170,12 @@ class ROM(object):
     #
 
     @property
-    def trainer_rom_start(self) -> bytes:
+    def trainer_rom_start(self):
         """The inclusive starting index of the trainer ROM."""
         return 16
 
     @property
-    def trainer_rom_stop(self) -> bytes:
+    def trainer_rom_stop(self):
         """The exclusive stopping index of the trainer ROM."""
         if self.has_trainer:
             return 16 + 512
@@ -183,37 +183,37 @@ class ROM(object):
             return 16
 
     @property
-    def trainer_rom(self) -> bytes:
+    def trainer_rom(self):
         """Return the trainer ROM of the ROM file."""
         return self.raw_data[self.trainer_rom_start:self.trainer_rom_stop]
 
     @property
-    def prg_rom_start(self) -> bytes:
+    def prg_rom_start(self):
         """The inclusive starting index of the PRG ROM."""
         return self.trainer_rom_stop
 
     @property
-    def prg_rom_stop(self) -> bytes:
+    def prg_rom_stop(self):
         """The exclusive stopping index of the PRG ROM."""
         return self.prg_rom_start + self.prg_rom_size * 2**10
 
     @property
-    def prg_rom(self) -> bytes:
+    def prg_rom(self):
         """Return the PRG ROM of the ROM file."""
         return self.raw_data[self.prg_rom_start:self.prg_rom_stop]
 
     @property
-    def chr_rom_start(self) -> bytes:
+    def chr_rom_start(self):
         """The inclusive starting index of the CHR ROM."""
         return self.prg_rom_stop
 
     @property
-    def chr_rom_stop(self) -> bytes:
+    def chr_rom_stop(self):
         """The exclusive stopping index of the CHR ROM."""
         return self.chr_rom_start + self.chr_rom_size * 2**10
 
     @property
-    def chr_rom(self) -> bytes:
+    def chr_rom(self):
         """Return the CHR ROM of the ROM file."""
         return self.raw_data[self.chr_rom_start:self.chr_rom_stop]
 

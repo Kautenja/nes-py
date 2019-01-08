2cb85100fdf0525a4937ed9ed2631de041b7b39a
Kautenja <ckfour@icloud.com>
2019-01-08 00:29:16 -0600

    document

--- nes_py/cpp/include/ppu.hpp
+++ nes_py/cpp/include/ppu.hpp
@@ -49,6 +49,8 @@ private:
     /// whether the PPU is on an even frame
     bool is_even_frame;
 
+    // Status
+
     /// whether the PPU is in vertical blanking mode
     bool is_vblank;
     /// whether sprite 0 has been hit (i.e., collision detection)
@@ -56,20 +58,20 @@ private:
 
     // Registers
 
+    /// the current data address to (read / write) (from / to)
     NES_Address data_address;
+    /// a temporary address register
     NES_Address temp_address;
+    /// the fine scrolling position
     NES_Byte fine_x_scroll;
+    /// TODO: doc
     bool is_first_write;
+    /// The address of the data buffer
     NES_Byte data_buffer;
-
+    /// the read / write address for the OAM memory (sprites)
     NES_Byte sprite_data_address;
 
-    // Setup flags and variables
-
-    /// TODO: doc
-    bool is_long_sprites;
-    /// whether the PPU is in the interrupt handler
-    bool is_interrupting;
+    // Mask
 
     /// whether the PPU is showing sprites
     bool is_showing_sprites;
@@ -80,11 +82,20 @@ private:
     /// whether the PPU is hiding the background along the edges
     bool is_hiding_edge_background;
 
+    // Setup flags and variables
+
+    /// TODO: doc
+    bool is_long_sprites;
+    /// whether the PPU is in the interrupt handler
+    bool is_interrupting;
+
+    /// TODO: doc
     enum CharacterPage {
         LOW,
         HIGH,
     } background_page, sprite_page;
 
+    /// The value to increment the data address by
     NES_Address data_address_increment;
 
     /// The internal screen data structure as a vector representation of a
@@ -92,54 +103,65 @@ private:
     /// the number of visible scan line dots
     NES_Pixel screen[VISIBLE_SCANLINES][SCANLINE_VISIBLE_DOTS];
 
-    /// Read a byte from OAM memory.
-    ///
-    /// @param address the address to read from OAM memory
-    /// @return the byte at the given address in OAM memory
-    ///
-    inline NES_Byte read_OAM(NES_Byte address) { return sprite_memory[address]; };
-
-    /// Write a byte to OAM memory.
-    ///
-    /// @param address the address to write to in OAM memory
-    /// @param value the byte to write to the given address
-    ///
-    inline void write_OAM(NES_Byte address, NES_Byte value) { sprite_memory[address] = value; };
-
 public:
-    /// Initialize a new PPU
+    /// Initialize a new PPU.
     PPU() : sprite_memory(64 * 4) { };
 
-    /// Perform a single cycle on the PPU
+    /// Perform a single cycle on the PPU.
     void cycle(PictureBus& bus);
 
-    /// Perform the number of PPU cycles that fit into a clock cycle (3)
+    /// Perform the number of PPU cycles that fit into a clock cycle (3).
     inline void step(PictureBus& bus) { cycle(bus); cycle(bus); cycle(bus); };
 
-    /// Reset the PPU
+    /// Reset the PPU.
     void reset();
 
-    /// Set the interrupt callback for the CPU
+    /// Set the interrupt callback for the CPU.
     void setInterruptCallback(std::function<void(void)> cb) { vblank_callback = cb; };
 
+    /// TODO: doc
     void doDMA(const NES_Byte* page_ptr);
 
-    //Callbacks mapped to CPU address space
-    //Addresses written to by the program
+    // MARK: Callbacks mapped to CPU address space
 
+    /// TODO: doc
     void control(NES_Byte ctrl);
+
+    /// TODO: doc
     void setMask(NES_Byte mask);
-    void setOAMAddress(NES_Byte address) { sprite_data_address = address; };
-    void setDataAddress(NES_Byte address);
+
+    /// TODO: doc
     void setScroll(NES_Byte scroll);
+
+    /// Return the value in the PPU status register.
+    NES_Byte getStatus();
+
+    /// TODO: doc
+    void setDataAddress(NES_Byte address);
+
+    /// Read data off the picture bus.
+    ///
+    /// @param bus the bus to read data off of
+    ///
+    NES_Byte getData(PictureBus& bus);
+
+    /// TODO: doc
     void setData(PictureBus& bus, NES_Byte data);
 
-    //Read by the program
+    /// TODO: doc
+    void setOAMAddress(NES_Byte address) { sprite_data_address = address; };
 
-    NES_Byte getStatus();
-    NES_Byte getData(PictureBus& bus);
-    NES_Byte getOAMData() { return read_OAM(sprite_data_address); };
-    void setOAMData(NES_Byte value) { write_OAM(sprite_data_address++, value); };
+    /// Read a byte from OAM memory at the sprite data address.
+    ///
+    /// @return the byte at the given address in OAM memory
+    ///
+    NES_Byte getOAMData() { return sprite_memory[sprite_data_address]; };
+
+    /// Write a byte to OAM memory at the sprite data address.
+    ///
+    /// @param value the byte to write to the given address
+    ///
+    void setOAMData(NES_Byte value) { sprite_memory[sprite_data_address++] = value; };
 
     /// Return a pointer to the screen buffer.
     NES_Pixel* get_screen_buffer() { return *screen; };

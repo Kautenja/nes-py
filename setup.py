93c7237d54c71a4509a68fc47f468ba5c3369adc
Kautenja <ckfour@icloud.com>
2019-05-07 16:31:51 -0500

    remove 3.8 dev build

--- setup.py
+++ setup.py
@@ -54,7 +54,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Topic :: Games/Entertainment',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Emulators',

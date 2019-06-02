c3af129c5ee61be5f75b3a9cc186c11b5c426dc7
Kautenja <ckfour@icloud.com>
2019-06-02 12:28:36 -0500

    bump version

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ LIB_NES_ENV = Extension(LIB_NAME,
 
 setup(
     name='nes_py',
-    version='7.0.1',
+    version='8.0.0',
     description='An NES Emulator and OpenAI Gym interface',
     long_description=README,
     long_description_content_type='text/markdown',

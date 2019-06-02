c231a3afdfb6942dc6b3995f0d49a0f502e2480d
Kautenja <ckfour@icloud.com>
2019-06-02 16:54:10 -0500

    add callback

--- nes_py/app/play_human.py
+++ nes_py/app/play_human.py
@@ -8,12 +8,13 @@ from .._image_viewer import ImageViewer
 _NOP = 0
 
 
-def play_human(env):
+def play_human(env: gym.Env, callback=None):
     """
     Play the environment using keyboard as a human.
 
     Args:
-        env (gym.Env): the initialized gym environment to play
+        env: the initialized gym environment to play
+        callback: a callback to receive output from the environment
 
     Returns:
         None
@@ -53,12 +54,16 @@ def play_human(env):
             # reset if the environment is done
             if done:
                 done = False
-                screen = env.reset()
-                viewer.show(screen)
+                state = env.reset()
+                viewer.show(env.unwrapped.screen)
             # unwrap the action based on pressed relevant keys
             action = keys_to_action.get(viewer.pressed_keys, _NOP)
-            screen, reward, done, info = env.step(action)
-            viewer.show(screen)
+            next_state, reward, done, _ = env.step(action)
+            viewer.show(env.unwrapped.screen)
+            # pass the observation data through the callback
+            if callback is not None:
+                callback(state, action, reward, done, next_state)
+            state = next_state
             # shutdown if the escape key is pressed
             if viewer.is_escape_pressed:
                 break

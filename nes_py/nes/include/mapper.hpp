04c5bd3da40a87cd8be23130a63e4d022dad30ef
Kautenja <kautencreations@gmail.com>
2019-07-16 07:37:02 -0500

    cleanup

--- nes_py/nes/include/mapper.hpp
+++ nes_py/nes/include/mapper.hpp
@@ -23,12 +23,11 @@ enum NameTableMirroring {
 
 /// An abstraction of a general hardware mapper for different NES cartridges
 class Mapper {
-
-protected:
+ protected:
     /// The cartridge this mapper associates with
     Cartridge& cartridge;
 
-public:
+ public:
     /// an enumeration of mapper IDs
     enum Type {
         NROM  = 0,
@@ -41,58 +40,58 @@ public:
     ///
     /// @param game a reference to a cartridge for the mapper to access
     ///
-    Mapper(Cartridge& game) : cartridge(game) { };
+    Mapper(Cartridge& game) : cartridge(game) { }
 
     /// Create a mapper based on given type, a game cartridge.
     ///
     /// @param game a reference to a cartridge for the mapper to access
-    /// @param mirroring_cb the callback to signify a change in mirroring mode
+    /// @param callback the callback to signify a change in mirroring mode
     /// @return a pointer to a mapper class based on the given game
     ///
-    static Mapper* create (Cartridge& game, std::function<void(void)> mirroring_cb);
+    static Mapper* create(Cartridge& game, std::function<void(void)> callback);
 
     /// Read a byte from the PRG RAM.
     ///
     /// @param address the 16-bit address of the byte to read
     /// @return the byte located at the given address in PRG RAM
     ///
-    virtual NES_Byte readPRG (NES_Address address) = 0;
+    virtual NES_Byte readPRG(NES_Address address) = 0;
 
     /// Write a byte to an address in the PRG RAM.
     ///
     /// @param address the 16-bit address to write to
     /// @param value the byte to write to the given address
     ///
-    virtual void writePRG (NES_Address address, NES_Byte value) = 0;
+    virtual void writePRG(NES_Address address, NES_Byte value) = 0;
 
     /// Read a byte from the CHR RAM.
     ///
     /// @param address the 16-bit address of the byte to read
     /// @return the byte located at the given address in CHR RAM
     ///
-    virtual NES_Byte readCHR (NES_Address address) = 0;
+    virtual NES_Byte readCHR(NES_Address address) = 0;
 
     /// Write a byte to an address in the CHR RAM.
     ///
     /// @param address the 16-bit address to write to
     /// @param value the byte to write to the given address
     ///
-    virtual void writeCHR (NES_Address address, NES_Byte value) = 0;
+    virtual void writeCHR(NES_Address address, NES_Byte value) = 0;
 
     /// Return the page pointer for the given address.
     ///
     /// @param address the address of the page pointer to get
     /// @return the page pointer at the given address
     ///
-    virtual const NES_Byte* getPagePtr (NES_Address address) = 0;
+    virtual const NES_Byte* getPagePtr(NES_Address address) = 0;
 
     /// Return the name table mirroring mode of this mapper.
     inline virtual NameTableMirroring getNameTableMirroring() {
         return static_cast<NameTableMirroring>(cartridge.getNameTableMirroring());
-    };
+    }
 
     /// Return true if this mapper has extended RAM, false otherwise.
-    inline bool hasExtendedRAM() { return cartridge.hasExtendedRAM(); };
+    inline bool hasExtendedRAM() { return cartridge.hasExtendedRAM(); }
 
 };
 
